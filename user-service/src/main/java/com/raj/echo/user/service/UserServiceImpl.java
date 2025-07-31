31821cf8db909d392bb0deee88d2b1c85e15722e
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-07-31 23:00:42 +0530

    changes

--- user-service/src/main/java/com/raj/echo/user/service/UserServiceImpl.java
+++ user-service/src/main/java/com/raj/echo/user/service/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.raj.echo.user.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
