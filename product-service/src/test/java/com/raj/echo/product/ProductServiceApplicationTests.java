5c1fcad0abca075dea1d4e6440afd25961673d94
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-07-31 21:34:20 +0530

    adding product service

---
+++ product-service/src/test/java/com/raj/echo/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.echo.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
