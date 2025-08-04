6dd1d352ec83b8df27df4b77a97eb35fb128245e
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-08-04 22:32:42 +0530

    service conf

--- order-service/src/main/java/com/raj/echo/order/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/raj/echo/order/service/OrderServiceImpl.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raj.echo.order.Dto.OrderItemRequest;
